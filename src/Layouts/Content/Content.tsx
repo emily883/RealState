4d18cba6f1a4904a7bd6601b139e3fc88858ffe9
Emily <emily.moraima.cruz@hotmail.com>
2023-03-30 23:26:15 -0400

    fixing background story on about component

--- src/Layouts/Content/Content.tsx
+++ src/Layouts/Content/Content.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from '../../components/About/About';
+import Services from '../../components/Services/Services';
 import style from './Content.module.scss';
 
 export default function Content() {
   return (
     <div className={style.ContentContainer}>
       <About />
+      <Services />
     </div>
   );
 }
